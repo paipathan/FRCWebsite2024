f61cc9890f9ff82abe394a0e6a08c23d4ad2cd10
Abhay Jani <abhayjani21@gmail.com>
2024-03-17 11:24:40 -0700

    issues w images so theyre all just sorta out there started contact us page fininished landing (for now)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
     document.addEventListener("DOMContentLoaded", function () {
-
         setTimeout(function(){
             document.querySelector('.center-title').classList.add('fade-in-logo');
             console.log('faded in')
@@ -12,12 +11,10 @@
                     document.querySelector('.team-name').classList.add('fade-in-text');
                     document.querySelector('.team-quote').classList.add('fade-in-text');
                     console.log('show number, name, quote')
-
                     }, 500)
                 }, 1000)
             }, 1000)
         }, 500)
-
     });
 
 document.querySelectorAll('.bottom-image').addEventListener("DOMContentLoaded", function() {
@@ -29,7 +26,25 @@ $('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
 
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
-  $('.dropdown').on('hide.bs.dropdown', function() {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+
+
+   // Function to handle the intersection observer callback
+   function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  // Create an intersection observer instance
+  const observer = new IntersectionObserver(handleIntersect, { root: null, threshold: 0.3 });
+
+  // Get all elements with the class 'fade-in'
+  const targets = document.querySelectorAll('.fade-in');
+
+  // Observe each target element
+  targets.forEach(target => {
+    observer.observe(target);
   });
